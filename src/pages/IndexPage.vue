2f1abd456e17ca29e9888a62896aaf19d0c945b9
Jangri <wechuks@gmail.com>
2025-03-21 21:22:46 +0200

    first commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,37 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const players = ref([])
+  const prizePool = ref(0)
+
+  // Player add
+  const addDialog = ref(false)
+  const addInput = ref("")
+
+  function toggleAddDialog(){
+    addInput.value = ""
+    addDialog.value = !addDialog.value
+  }
+
+  function addPlayer(){
+    players.value.push({
+      name: addInput.value,
+      bounty: 15,
+      winnings: 0
+    })
+    prizePool.value += 15
+    toggleAddDialog()
+  }
+</script>
+
 <template>
+  <q-dialog v-model="addDialog">
+    <q-card>
+      <q-input v-model="addInput" label="Name" />
+      <q-btn @click="addPlayer" label="Add" />
+    </q-card>
+  </q-dialog>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-btn @click="toggleAddDialog" label="Add Player" />
   </q-page>
 </template>
-
-<script setup>
-//
-</script>
